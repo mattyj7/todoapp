565b5551ccedd882190f197afb4499296249fdcf
Matthew Jackson <120997192+mattyj7@users.noreply.github.com>
2024-02-10 12:59:10 -0700

    Updated api model - added title

--- backend/api/models.py
+++ backend/api/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Note(models.Model):
+    title = models.CharField(max_length=255)
     body = models.TextField(null=True, blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
