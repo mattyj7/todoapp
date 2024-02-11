494d318ffd5134aec30f5328e72c0d8dfe917aac
Matthew Jackson <120997192+mattyj7@users.noreply.github.com>
2024-02-11 11:43:26 -0700

    Updated date function logic

--- frontend/src/components/AddButton.js
+++ frontend/src/components/AddButton.js
@@ -4,7 +4,7 @@ import { ReactComponent as AddIcon } from "../assets/add.svg";
 
 const AddButton = () => {
   return (
-    <div>
+    <div className="add-button">
       <Link to="/note/create/" className="floating-button">
         <AddIcon />
       </Link>
--- frontend/src/components/ListItem.js
+++ frontend/src/components/ListItem.js
@@ -5,11 +5,17 @@ const ListItem = ({ note }) => {
   const lastUpdated = (rawDate) => {
     const createdDate = new Date(rawDate);
     const currentDate = new Date();
+    const createdDay = createdDate.getDate();
+    const currentDay = currentDate.getDate();
     let lastUpdatedLength = null;
+
     const timeDifference = currentDate - createdDate;
     const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-    if (daysDifference === 0) {
+
+    if (daysDifference === 0 && createdDay === currentDay) {
       lastUpdatedLength = "Today";
+    } else if (currentDay - createdDay === 1) {
+      lastUpdatedLength = "Yesterday";
     } else if (daysDifference > 0 && daysDifference < 10) {
       lastUpdatedLength = daysDifference + " Days";
     } else if (daysDifference > 10 && daysDifference < 30) {
