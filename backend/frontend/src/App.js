69f9920d95d96ba6784c04e40c37cd67eecc2f22
Matthew Jackson <120997192+mattyj7@users.noreply.github.com>
2024-02-13 20:06:46 -0700

    Updated app functionality to add calendar

--- backend/frontend/src/App.js
+++ backend/frontend/src/App.js
@@ -5,22 +5,22 @@ import Header from "./components/Header";
 import NotesListPage from "./pages/NotesListPage";
 import NotePage from "./pages/NotePage";
 import CreateNotePage from "./pages/CreateNotePage";
-import CalendarPage from "./pages/CalendarPage";
+import CalendarPage from "./components/Calendar";
 
 function App() {
   return (
     <Router>
       <div className="container dark">
         <div className="app">
+          <Header />
           <div className="app-todo">
-            <Header />
             <Routes>
               <Route path="/notes" exact Component={NotesListPage} />
               <Route path="/notes/:noteId" Component={NotePage} />
               <Route path="/note/create" Component={CreateNotePage} />
             </Routes>
+            <CalendarPage />
           </div>
-          <CalendarPage />
         </div>
       </div>
     </Router>
